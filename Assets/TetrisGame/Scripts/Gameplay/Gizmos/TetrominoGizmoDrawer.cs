76611a81dc1a94fd4e8528fe5e44561ebe2575f9
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2025-03-21 20:24:38 +0100

    Spawn with cubes

--- Assets/TetrisGame/Scripts/Gameplay/Gizmos/TetrominoGizmoDrawer.cs
+++ Assets/TetrisGame/Scripts/Gameplay/Gizmos/TetrominoGizmoDrawer.cs
@@ -7,25 +7,20 @@ public class TetrominoGizmoDrawer : MonoBehaviour
 {
     private void OnDrawGizmos()
     {
-        // Aby rysować w Scene View, sprawdzamy czy gra jest uruchomiona.
         if (!Application.isPlaying) return;
         
         var world = World.DefaultGameObjectInjectionWorld;
         if (world == null) return;
         
         var entityManager = world.EntityManager;
-        // Zapytanie o encje, które mają LocalTransform oraz dynamiczny bufor TetrominoBlock.
         var query = entityManager.CreateEntityQuery(typeof(LocalTransform), typeof(GameLogic.TetrominoBlock));
         var entities = query.ToEntityArray(Unity.Collections.Allocator.Temp);
         
         foreach (var entity in entities)
         {
-            // Pobieramy transform tetromino
             var localTransform = entityManager.GetComponentData<LocalTransform>(entity);
-            // Pobieramy bufor bloków tetromino
             var buffer = entityManager.GetBuffer<GameLogic.TetrominoBlock>(entity);
             
-            // Rysujemy każdy blok w odpowiedniej pozycji (transform + offset)
             foreach (var block in buffer)
             {
                 Vector3 blockPos = new Vector3(
@@ -33,7 +28,6 @@ public class TetrominoGizmoDrawer : MonoBehaviour
                     localTransform.Position.y + block.Offset.y,
                     localTransform.Position.z);
                 Gizmos.color = Color.green;
-                // Rysujemy sześcian o wielkości nieco mniejszej niż komórka, by lepiej widzieć odstępy
                 Gizmos.DrawCube(blockPos, Vector3.one * 0.95f);
             }
         }
--- Assets/TetrisGame/Scripts/Gameplay/TetrominoSpawnSystem.cs
+++ Assets/TetrisGame/Scripts/Gameplay/TetrominoSpawnSystem.cs
@@ -2,10 +2,8 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.NetCode;
 using Unity.Transforms;
 using UnityEngine;
-
 
 namespace GameLogic
 {
@@ -15,82 +13,136 @@ namespace GameLogic
     {
         public int2 Offset;
     }
+    
+    public struct TetrominoBlockLink : IComponentData
+    {
+        public Entity Container;
+        public int BlockIndex;
+    }
+    
     public struct TetrominoPlaced : IComponentData { }
     
-    public struct LocalToParent : IComponentData { }
+    public struct VisualSpawnedTag : IComponentData { }
     
+    public struct VisualOffset : IComponentData
+    {
+        public float3 Value;
+    }
 
     [WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
-    public partial class TetrominoSpawnSystem : SystemBase
+    [BurstCompile]
+    public partial class TetrominoSpawnAndFollowSystem : SystemBase
     {
         public float SpawnInterval = 3f;
         private float _timeSinceLastSpawn;
 
         protected override void OnUpdate()
         {
+            var containerTransformsLookup = GetComponentLookup<LocalTransform>(true);
+            var containerBlockBuffers = GetBufferLookup<TetrominoBlock>(true);
+            
+            NativeParallelHashMap<Entity, LocalTransform> parentTransformMap = 
+                new NativeParallelHashMap<Entity, LocalTransform>(100, Allocator.TempJob);
+
+            Entities
+                .WithReadOnly(containerTransformsLookup)
+                .WithAll<TetrominoContainer>()
+                .ForEach((Entity entity) =>
+                {
+                    parentTransformMap.TryAdd(entity, containerTransformsLookup[entity]);
+                }).Run();
+
+            Dependency = Entities
+                .WithReadOnly(parentTransformMap)
+                .WithReadOnly(containerBlockBuffers)
+                .WithBurst()
+                .ForEach((ref LocalTransform blockLocalTransform, in TetrominoBlockLink link) =>
+                {
+                    if (parentTransformMap.TryGetValue(link.Container, out LocalTransform parentTransform))
+                    {
+                        DynamicBuffer<TetrominoBlock> blockBuffer = containerBlockBuffers[link.Container];
+                        if (link.BlockIndex >= 0 && link.BlockIndex < blockBuffer.Length)
+                        {
+                            int2 offset = blockBuffer[link.BlockIndex].Offset;
+                            blockLocalTransform.Position = parentTransform.Position + new float3(offset.x, offset.y, 0f);
+                        }
+                    }
+                }).ScheduleParallel(Dependency);
+
+            Dependency.Complete();
+            parentTransformMap.Dispose();
+            
             float deltaTime = SystemAPI.Time.DeltaTime;
             _timeSinceLastSpawn += deltaTime;
-            
+
             EntityQuery fallingQuery = GetEntityQuery(ComponentType.ReadOnly<FallingTetromino>());
-            if (fallingQuery.CalculateEntityCount() > 0)
-                return;
-
-            if (_timeSinceLastSpawn < SpawnInterval)
-                return;
-
-            _timeSinceLastSpawn = 0f;
-            var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-            if (SystemAPI.TryGetSingleton<PrefabElement>(out var prefabElement))
+            if (fallingQuery.CalculateEntityCount() == 0 && _timeSinceLastSpawn >= SpawnInterval)
             {
-                using (var ecb = new EntityCommandBuffer(Allocator.Temp))
+                _timeSinceLastSpawn = 0f;
+                var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+                if (SystemAPI.TryGetSingleton<PrefabElement>(out var prefabElement))
                 {
-                    Entity container = ecb.CreateEntity();
-                    ecb.AddComponent(container, new TetrominoContainer());
-                    ecb.AddComponent(container, new FallingTetromino());
-                    ecb.AddComponent(container, new FallSpeed { Value = 2f });
-                    ecb.AddComponent(container, new LocalTransform
+                    using (var ecb = new EntityCommandBuffer(Allocator.Temp))
                     {
-                        Position = new float3(5f, 18f, 0f),
-                        Rotation = quaternion.identity,
-                        Scale = 1f
-                    });
-                    ecb.AddComponent(container, new LocalToWorld());
-                    
-                    var blockBuffer = ecb.AddBuffer<TetrominoBlock>(container);
-                    
-                    NativeList<int2> offsets = new NativeList<int2>(Allocator.Temp);
-                    int shapeIndex = UnityEngine.Random.Range(0, 2);
-                    if (shapeIndex == 0)
-                    {
-                        // Kwadrat 2x2
-                        offsets.Add(new int2(0, 0));
-                        offsets.Add(new int2(1, 0));
-                        offsets.Add(new int2(0, 1));
-                        offsets.Add(new int2(1, 1));
+                        Entity container = ecb.CreateEntity();
+                        ecb.AddComponent(container, new TetrominoContainer());
+                        ecb.AddComponent(container, new FallingTetromino());
+                        ecb.AddComponent(container, new FallSpeed { Value = 2f });
+                        
+                        float3 containerStartPos = new float3(5f, 18f, 0f);
+                        ecb.AddComponent(container, new LocalTransform
+                        {
+                            Position = containerStartPos,
+                            Rotation = quaternion.identity,
+                            Scale = 1f
+                        });
+                        ecb.AddComponent(container, new LocalToWorld());
+                        
+                        DynamicBuffer<TetrominoBlock> blockBuffer = ecb.AddBuffer<TetrominoBlock>(container);
+                        
+                        NativeList<int2> offsets = new NativeList<int2>(Allocator.Temp);
+                        int shapeIndex = UnityEngine.Random.Range(0, 2);
+                        if (shapeIndex == 0)
+                        {
+                            //square   2x2
+                            offsets.Add(new int2(0, 0));
+                            offsets.Add(new int2(1, 0));
+                            offsets.Add(new int2(0, 1));
+                            offsets.Add(new int2(1, 1));
+                        }
+                        else
+                        {
+                            // line 4x1
+                            offsets.Add(new int2(0, 0));
+                            offsets.Add(new int2(1, 0));
+                            offsets.Add(new int2(2, 0));
+                            offsets.Add(new int2(3, 0));
+                        }
+                        
+                        for (int i = 0; i < offsets.Length; i++)
+                        {
+                            int2 offset = offsets[i];
+                            blockBuffer.Add(new TetrominoBlock { Offset = offset });
+                            Entity blockEntity = ecb.Instantiate(prefabElement.Value);
+                            ecb.AddComponent(blockEntity, new TetrominoBlockLink { Container = container, BlockIndex = i });
+                            ecb.SetComponent(blockEntity, new LocalTransform
+                            {
+                                Position = containerStartPos + new float3(offset.x, offset.y, 0f),
+                                Rotation = quaternion.identity,
+                                Scale = 1f
+                            });
+                        }
+                        offsets.Dispose();
+
+                        Debug.Log($"Tetromino spawned with container Entity {container.Index}");
+                        ecb.Playback(entityManager);
                     }
-                    else
-                    {
-                        // Linia 4x1
-                        offsets.Add(new int2(0, 0));
-                        offsets.Add(new int2(1, 0));
-                        offsets.Add(new int2(2, 0));
-                        offsets.Add(new int2(3, 0));
-                    }
-                    
-                    for (int i = 0; i < offsets.Length; i++)
-                    {
-                        blockBuffer.Add(new TetrominoBlock { Offset = offsets[i] });
-                    }
-                    offsets.Dispose();
-                    
-                    Debug.Log($"Tetromino spawned with container Entity {container.Index}");
-                    ecb.Playback(entityManager);
                 }
-            }
-            else
-            {
-                Debug.LogWarning("PrefabElement singleton not found!");
+                else
+                {
+                    Debug.LogWarning("PrefabElement singleton not found!");
+                }
             }
         }
     }
